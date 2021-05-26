781b04f2a62f1c48a089e3aac5d9ac273a4728a6
VitorLSBrandao <vitor.brandao@eduzz.com>
2021-05-25 22:01:01 -0300

    Add env and settings to separate restricted data

---
+++ github-api-search/src/settings.ts
@@ -0,0 +1,4 @@
+export const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET || '';
+export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID || '';
+export const CALLBACK_URL = process.env.REACT_APP_CALLBACK_URL || '';
+export const GITHUB_URL = process.env.REACT_APP_GITHUB_URL || '';
