7f8bdf193460582df134161ef092f4d2387dda56
VitorLSBrandao <vitor.brandao@eduzz.com>
2021-05-25 22:04:57 -0300

    Adds credentials fetched from settings to the basic route of axios.

--- github-api-search/src/services/api.ts
+++ github-api-search/src/services/api.ts
@@ -1,3 +1,9 @@
 import axios from 'axios';
+import { CLIENT_SECRET } from '../settings';
 
-export const api = axios.create({ baseURL: 'https://api.github.com/users' });
+export const api = axios.create({
+  baseURL: 'https://api.github.com/users',
+  headers: {
+    Authorization: CLIENT_SECRET,
+  },
+});
